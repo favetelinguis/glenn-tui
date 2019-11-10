9f08c106c1cd0030664181b49c2373f727018413
Henrik Larsson <reg@henkelarsson.se>
2019-11-10 20:39:47 +0100

    Create config path when trying to read clients.json

--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,12 @@
 use config::{Config, ConfigError, Environment, File};
 use failure::ResultExt;
 use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
+use std::fs;
+
+const FILE_NAME: &str = "clients";
+const CONFIG_DIR: &str = ".config";
+const APP_CONFIG_DIR: &str = "glenn";
 
 #[derive(Debug, Deserialize)]
 pub struct Role {
@@ -24,10 +30,7 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        let mut config_path = dirs::home_dir().unwrap();
-        config_path.push(".config");
-        config_path.push("glenn");
-        config_path.push("clients");
+        let mut config_path = get_or_build_paths();
 
         let mut s = Config::new();
         s.merge(File::with_name("config/default").required(true))?;
@@ -35,4 +38,26 @@ impl Settings {
 
         s.try_into()
     }
+}
+fn get_or_build_paths() -> PathBuf {
+    match dirs::home_dir() {
+        Some(home) => {
+            let path = Path::new(&home);
+            let home_config_dir = path.join(CONFIG_DIR);
+            let app_config_dir = home_config_dir.join(APP_CONFIG_DIR);
+
+            if !home_config_dir.exists() {
+                fs::create_dir(&home_config_dir).unwrap();
+            }
+
+            if !app_config_dir.exists() {
+                fs::create_dir(&app_config_dir).unwrap();
+            }
+
+            let config_file_path = &app_config_dir.join(FILE_NAME);
+
+            Ok(config_file_path.to_path_buf())
+        }
+        None => Err("No $HOME directory found for client config"),
+    }.unwrap()
 }
